2441e984e8b79a506abaafaee7b296e836fe3749
inzamam malik <malikasinger@gmail.com>
2015-11-17 08:12:42 -0800

    series sms send successfully, now trying to add toast that if sms send or not

--- platforms/android/assets/www/views/sendSms/sendSms.js
+++ platforms/android/assets/www/views/sendSms/sendSms.js
@@ -1,16 +1,38 @@
 angular.module("starter")
-  .controller("sendSmsController",function($scope,$cordovaSMS){
+  .controller("sendSmsController",function($scope,$cordovaSms){
 
-$scope.send = function(){
+    $scope.data = {};
+    var options = {
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+      android: {
+        //intent: 'INTENT'  // send SMS with the native android SMS messaging
+        intent: '' // send SMS without open any other app
+      }
+    };
 
-  $scope.data = {};
+    $scope.send = function(){
+      $scope.data.count--;
 
-  $cordovaSms
+
+    $cordovaSms
     .send($scope.data.mobileNumber, $scope.data.text , options)
     .then(function() {
-      // Success! SMS was sent
+      console.log("// Success! SMS was sent");
+        $scope.data.mobileNumber++;
+        if($scope.data.count){
+
+          $scope.send();
+
+        }
     }, function(error) {
+        console.log("// An error occurred");
       // An error occurred
+        $scope.data.mobileNumber++;
+        if($scope.data.count){
+
+          $scope.send();
+          
+        }
     });
 }
 
--- www/views/sendSms/sendSms.js
+++ www/views/sendSms/sendSms.js
@@ -1,21 +1,40 @@
 angular.module("starter")
-  .controller("sendSmsController",function($scope/*,$cordovaSMS*/){
+  .controller("sendSmsController", function ($scope, $cordovaSms) {
 
     $scope.data = {};
+    var options = {
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+      android: {
+        //intent: 'INTENT'  // send SMS with the native android SMS messaging
+        intent: '' // send SMS without open any other app
+      }
+    };
 
     $scope.send = function(){
+      $scope.data.count--;
 
-      for(var i=0; i < $scope.data.count; i++){
 
-        console.log($scope.data.mobileNumber + i);
-      }
-    /*$cordovaSms
+      $cordovaSms
     .send($scope.data.mobileNumber, $scope.data.text , options)
     .then(function() {
-      // Success! SMS was sent
+          console.log("// Success! SMS was sent");
+        
+          $scope.data.mobileNumber++;
+          if ($scope.data.count) {
+
+            $scope.send();
+
+          }
     }, function(error) {
+          console.log("// An error occurred");
       // An error occurred
-    });*/
+          $scope.data.mobileNumber++;
+          if ($scope.data.count) {
+
+            $scope.send();
+
+          }
+        });
 }
 
 });
