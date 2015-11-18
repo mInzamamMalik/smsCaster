357be8ed0d658fd378c9871a91e67ea69efe68e6
inzamam malik <malikasinger@gmail.com>
2015-11-18 19:09:52 +0500

    SMS PLUGIN ADDED   (PARTIALLY)

--- www/views/sendSms/sendSms.js
+++ www/views/sendSms/sendSms.js
@@ -1,6 +1,17 @@
 angular.module("starter")
-  .controller("sendSmsController",function($scope){
+  .controller("sendSmsController",function($scope,$cordovaSMS){
 
+$scope.send = function(){
 
+  $scope.data = {};
+
+  $cordovaSms
+    .send($scope.data.mobileNumber, $scope.data.text , options)
+    .then(function() {
+      // Success! SMS was sent
+    }, function(error) {
+      // An error occurred
+    });
+}
 
 });
