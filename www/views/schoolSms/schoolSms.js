31df729189032701f35387407c7c9c2071378245
kamransiddiqui <sm_siddique@hotmail.com>
2015-11-19 21:12:50 +0500

    xls table completed

--- www/views/schoolSms/schoolSms.js
+++ www/views/schoolSms/schoolSms.js
@@ -1,4 +1,35 @@
-angular.module("starter").controller("schoolSmsController",function($scope){
+angular.module("starter")
+  .controller("schoolSmsController",function($scope,$cordovaSms){
+
+    $scope.getxsldata = function ( ) {
+      alasql('SELECT * FROM XLS("sms.xls",{headers:true})',[],function(data){
+        $scope.rec=data;
+      });
+    };
+
+    $scope.dosend = function(){
+      console.log($scope.rec);
+      for(var i=0; i < $scope.rec.length; i++ ){
+        $scope.send($scope.rec[i].contactno,"dafasdf");
+      }
+
+    };
+
+
+
+    $scope.send = function(num,text){
+      var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+          //intent: 'INTENT'  // send SMS with the native android SMS messaging
+          intent: '' // send SMS without open any other app
+        }
+      };
+      $cordovaSms
+        .send(num, text, options)
+    };
+
+
 
 
 
